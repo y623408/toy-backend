cc33416ffcb44a46554ec10941505fcc43caba02
Yashraj <y623408@gmail.com>
2024-03-06 09:56:09 +0530

    cors changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const mongoose = require('mongoose');
-const connectDB = require('./DBConnection/DbConfig');
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const mongoose = require("mongoose");
+const connectDB = require("./DBConnection/DbConfig");
 
 const userRoute = require("./Routes/UserRouter");
 const PORT = process.env.PORT || 3500;
@@ -17,15 +17,19 @@ connectDB;
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded form data
-app.use(express.urlencoded({ extended: false/* true, limit:'10mb'  */}));
+app.use(express.urlencoded({ extended: false /* true, limit:'10mb'  */ }));
 
-// built-in middleware for json 
-app.use(express.json({/* limit:'10mb' */}));
+// built-in middleware for json
+app.use(
+  express.json({
+    /* limit:'10mb' */
+  })
+);
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.use('/', userRoute);
+app.use("/", userRoute);
 /* 
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
@@ -37,20 +41,22 @@ app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
  */
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, "0.0.0.0", () =>
+    console.log(`Server running on port ${PORT}`)
+  );
 });
 
 /* const express = require("express");
